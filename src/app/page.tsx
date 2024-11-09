146de5f7f5e03ea94420ded67eeeb36ecf8f8271
Ruisheng <1639300-ruisheng95@users.noreply.gitlab.com>
2024-11-10 07:07:59 +0800

    amend page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 "use client";
 import {
   Box,
+  Button,
   Container,
   FormControl,
   InputLabel,
-  ListItemButton,
-  ListItemText,
   MenuItem,
   Select,
   Skeleton,
   Typography,
 } from "@mui/material";
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Markdown from "react-markdown";
 import { FixedSizeList } from "react-window";
 
@@ -23,16 +22,12 @@ interface ReactWindowRowProps {
 
 interface ReactWindowRowItemData {
   logs: string[];
-  setResponse: Dispatch<SetStateAction<string>>;
-  setLoading: Dispatch<SetStateAction<boolean>>;
-  selectedIndex: number;
-  setSelectedIndex: Dispatch<SetStateAction<number>>;
 }
 
 const ReactWindowRow = ({ data, style, index }: ReactWindowRowProps) => {
   return (
     <Box key={index} style={style}>
-      <ListItemButton
+      {/* <ListItemButton
         selected={data.selectedIndex === index}
         onClick={() => {
           const params = new Proxy(
@@ -100,13 +95,13 @@ const ReactWindowRow = ({ data, style, index }: ReactWindowRowProps) => {
         }}
       >
         <ListItemText primary={data.logs[index]} />
-      </ListItemButton>
+      </ListItemButton> */}
+      <Typography>{data.logs[index]}</Typography>
     </Box>
   );
 };
 
 const Home = () => {
-  const [selectedIndex, setSelectedIndex] = useState(0);
   const [response, setResponse] = useState("");
   const [loading, setLoading] = useState(false);
   const [logs, setLogs] = useState<string[]>([]);
@@ -125,38 +120,104 @@ const Home = () => {
       <Container
         sx={{ pt: 2, display: "flex", flexDirection: "column", gap: 2 }}
       >
-        <FormControl>
-          <InputLabel>Log Type</InputLabel>
-          <Select
-            label="Log Type"
-            value={selectLogType}
-            onChange={(event) => {
-              setSelectLogType(event.target.value);
-              fetch(`/${event.target.value}`)
-                .then((r) => r.text())
-                .then((text) => {
-                  setLogs(text.split("\n"));
-                });
+        <Box sx={{ display: "flex", gap: 2 }}>
+          <FormControl sx={{ flex: 1 }}>
+            <InputLabel>Log Type</InputLabel>
+            <Select
+              label="Log Type"
+              value={selectLogType}
+              onChange={(event) => {
+                setSelectLogType(event.target.value);
+                fetch(`/${event.target.value}`)
+                  .then((r) => r.text())
+                  .then((text) => {
+                    setLogs(text.split("\n"));
+                  });
+              }}
+            >
+              <MenuItem value={"sshd_sample.txt"}>[SSHD] Full Logs (2000 lines)</MenuItem>
+              <MenuItem value={"mix_good_bad_sshd_log.txt"}>
+                [SSHD] Mixed Good Bad Logs (12 lines)
+              </MenuItem>
+            </Select>
+          </FormControl>
+          <Button
+            variant="contained"
+            onClick={() => {
+              const params = new Proxy(
+                new URLSearchParams(window.location.search),
+                {
+                  // @ts-expect-error prop type is string | symbol
+                  get: (searchParams, prop) => searchParams.get(prop),
+                }
+              );
+              // @ts-expect-error if query param test, go test mode
+              if (params.test) {
+                setResponse("");
+                setLoading(true);
+                setTimeout(function () {
+                  setResponse("@@");
+                  setLoading(false);
+                }, 2000);
+              } else {
+                setResponse("");
+                setLoading(true);
+                const prompt =
+                  "Is there anything suspicious on the following logs: \n";
+                const content = logs.join("\n");
+                fetch(
+                  "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyATrtI1emJyYi7gxvdJRbEKOeWI0u2lELg",
+                  {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                      system_instruction: {
+                        parts: {
+                          text: "The answer should summarize suspicious and non-suspicious activities (if any)",
+                        },
+                      },
+                      safetySettings: [
+                        {
+                          category: "HARM_CATEGORY_DANGEROUS_CONTENT",
+                          threshold: "BLOCK_NONE",
+                        },
+                      ],
+                      contents: [
+                        {
+                          parts: [
+                            {
+                              text: `${prompt} ${content}`,
+                            },
+                            {
+                              text: "\nFinally, provide a summary on the incident with actionable next steps."
+                            }
+                          ],
+                        },
+                      ],
+                    }),
+                  }
+                )
+                  .then((response) => response.json())
+                  .then((json) => {
+                    setResponse(json.candidates[0].content.parts[0].text);
+                    setLoading(false);
+                  });
+              }
             }}
           >
-            <MenuItem value={"sshd_sample.txt"}>Full Logs</MenuItem>
-            <MenuItem value={"mix_good_bad_sshd_log.txt"}>
-              Mixed Good Bad Logs
-            </MenuItem>
-          </Select>
-        </FormControl>
+            AI
+          </Button>
+        </Box>
         <Box sx={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: 2 }}>
           <FixedSizeList
             height={600}
             itemCount={logs.length}
-            itemSize={80}
+            itemSize={75}
             width={600}
             itemData={{
               logs,
-              setResponse,
-              setLoading,
-              selectedIndex,
-              setSelectedIndex,
             }}
           >
             {ReactWindowRow}
